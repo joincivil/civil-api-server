bebb27654caf59c3d55c447b8c2750f99ece400d
Dan Kinsley <dan@mgemi.com>
2019-06-20 12:21:31 -0400

    don't actually validate expectedReceiver in ETH payments

--- pkg/payments/ethereum.go
+++ pkg/payments/ethereum.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	log "github.com/golang/glog"
 	"github.com/joincivil/civil-api-server/pkg/storefront"
 )
 
@@ -57,7 +58,10 @@ func (s *EthereumService) ValidateTransaction(transactionID string, expectedRece
 	// ensure that we are actually transferring to the correct address
 	// this is to prevent trying to game the system but sending a tx from a payment that wasn't actually to them
 	if data.To().String() != receiverAddr.String() {
-		return nil, fmt.Errorf("transaction sent to %v but was expecting %v", data.To().String(), receiverAddr.String())
+
+		// TODO(dankins): don't actually return an error until this is fully implemented
+		log.Errorf("transaction sent to %v but was expecting %v. continuing on until the expectedReceiver logic is implemented.", data.To().String(), receiverAddr.String())
+		// return nil, fmt.Errorf("transaction sent to %v but was expecting %v", data.To().String(), receiverAddr.String())
 	}
 	// convert the transction amount from wei to ether
 	var ether = new(big.Float).SetInt(data.Value())
