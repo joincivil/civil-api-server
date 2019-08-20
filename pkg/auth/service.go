daf2321de7d6e586460c86292e72c0810910083a
Peter Ng <peter@joincivil.com>
2019-08-20 11:52:19 -0400

    chore: move signature code to go-common
    moved it out for common usage. this cleans it out from this repo and
    switches it over to the go-common version.

--- pkg/auth/service.go
+++ pkg/auth/service.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/joincivil/go-common/pkg/email"
+	"github.com/joincivil/go-common/pkg/eth"
 	cpersist "github.com/joincivil/go-common/pkg/persistence"
 
 	"github.com/joincivil/civil-api-server/pkg/users"
@@ -134,7 +135,7 @@ func NewAuthService(userService *users.UserService, tokenGenerator *utils.JwtTok
 // SignupEth validates the Signature input then creates a User for that address
 func (s *Service) SignupEth(input *users.SignatureInput) (*LoginResponse, error) {
 
-	err := VerifyEthChallengeAndSignature(ChallengeRequest{
+	err := eth.VerifyEthChallengeAndSignature(eth.ChallengeRequest{
 		ExpectedPrefix: "Sign up with Civil",
 		GracePeriod:    defaultGracePeriod,
 		InputAddress:   input.Signer,
@@ -228,7 +229,7 @@ func (s *Service) SignupEmailConfirm(signupJWT string) (*LoginResponse, error) {
 
 // LoginEth creates a new user for the address in the Ethereum signature
 func (s *Service) LoginEth(input *users.SignatureInput) (*LoginResponse, error) {
-	err := VerifyEthChallengeAndSignature(ChallengeRequest{
+	err := eth.VerifyEthChallengeAndSignature(eth.ChallengeRequest{
 		ExpectedPrefix: "Log in to Civil",
 		GracePeriod:    defaultGracePeriod,
 		InputAddress:   input.Signer,
